fcf4ca24d7e4a61f0d939611ef3563e41f5ccac3
ranggirexa <ranggirexa@gmail.com>
2022-03-29 01:21:04 +0700

    format currency

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -1,10 +1,10 @@
 module FormatHelper
-  
+
   def format_price(price)
-    # TODO: Format the price. 
+    # TODO: Format the price. #done
     # - Display a dollar sign
     # - Display two decimal places
-    price
+    number_to_currency(price, { unit: "$ ", delimiter: ".", separator: ",", precision: 2 })
   end
 
 end
